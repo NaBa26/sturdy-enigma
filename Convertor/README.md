64f90fac70a37dce3e3e34dc300c1ccd31d7db74
NaBa26 <namankumar777@gmail.com>
2024-04-10 21:50:37 +0530

    Initial Commit

---
+++ Convertor/README.md
@@ -0,0 +1,6 @@
+# Base Convertor
+
+Convert from Hexadecimal, Decimal, Binary or Octal to any of the other three.
+
+## Technologies Used
+HTML,CSS and JS
---
+++ Convertor/convertor.js
@@ -0,0 +1,6 @@
+const selectedBase = document.getElementById('selectedBase').value;
+const enteredValue = document.getElementById('enteredValue').value;
+const resultBase = document.getElementById('resultBase').value;
+const button = document.getElementById('button');
+
+let value = parseInt(enteredValue);
