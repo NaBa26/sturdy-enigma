6c635908d0d0fede7b93572d9caf30b37dd43c88
NaBa26 <namankumar777@gmail.com>
2024-05-07 23:03:50 +0530

    Committed

--- stopwatch/src/content.jsx
+++ stopwatch/src/content.jsx
@@ -1,40 +1,65 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 
-function Stopwatch(){
+function Stopwatch() {
+  const [time, setTime] = useState({ hour: 0, min: 0, sec: 0 });
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null);
 
-  const [time, setTime] = useState({hour:"00",min:"00",sec:"00"});
-  
-  function isRunning(){
-    if(time.hour=="00" && time.sec=="00" && time.min=="00")
-    return false;
-  }
+  const startStopwatch = () => {
+    if (isRunning) {
+      clearInterval(intervalRef.current);
+    } else {
+      intervalRef.current = setInterval(() => {
+        setTime((prevTime) => {
+          let { hour, min, sec } = prevTime;
+          sec++;
+          if (sec === 60) {
+            sec = 0;
+            min++;
+          }
+          if (min === 60) {
+            min = 0;
+            hour++;
+          }
+          return { hour, min, sec };
+        });
+      }, 1000);
+    }
+    setIsRunning((prevIsRunning) => !prevIsRunning);
+  };
 
-  return(
+  const resetStopwatch = () => {
+    clearInterval(intervalRef.current);
+    setTime({ hour: 0, min: 0, sec: 0 });
+    setIsRunning(false);
+  };
+
+  return (
     <div className="container">
-    <div className="innerContainer">
-      <div className="hour">
-        <div className="hourLabel">
-          <label>Hours</label>
+      <div className="innerContainer">
+        <div className="hour">
+          <div className="hourLabel">
+            <label>Hours</label>
+          </div>
+          <output>{time.hour < 10 ? `0${time.hour}` : time.hour}</output>
         </div>
-        <output>{time.hour}</output>
-      </div>
-      <div className="min">
-        <div className="minLabel">
-          <label>Minutes</label>
+        <div className="min">
+          <div className="minLabel">
+            <label>Minutes</label>
+          </div>
+          <output>{time.min < 10 ? `0${time.min}` : time.min}</output>
         </div>
-        <output>{time.hour}</output>
-      </div>
-      <div className="sec">
-        <div className="secLabel">
-          <label>Seconds</label>
+        <div className="sec">
+          <div className="secLabel">
+            <label>Seconds</label>
+          </div>
+          <output>{time.sec < 10 ? `0${time.sec}` : time.sec}</output>
         </div>
-        <output>{time.hour}</output>
       </div>
-    </div>
-    <div className="buttonContainer">
-    <button>Start</button>
-    <button>Reset</button>
-    </div>
+      <div className="buttonContainer">
+        <button onClick={startStopwatch}>{isRunning ? 'Stop' : 'Start'}</button>
+        <button onClick={resetStopwatch}>Reset</button>
+      </div>
     </div>
   );
 }
